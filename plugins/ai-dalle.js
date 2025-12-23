fc369ffd32b99862d8039353cbaaf9e2434303a4
Danielrxz <danielrodigome@gmail.com>
2025-12-23 07:35:08 -0700

    Add files via upload

---
+++ plugins/ai-dalle.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const handler = async (m, { conn, args }) => {
+    if (!args[0]) {
+        await conn.reply(m.chat, `${emoji} Proporciona una descripción para generar la imagen.`, m);
+        return;
+    }
+
+    const prompt = args.join(' ');
+    const apiUrl = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${prompt}`;
+
+    try {
+        conn.reply(m.chat, `${emoji2} Espera un momento...`, m)
+
+        const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+
+        await conn.sendMessage(m.chat, { image: Buffer.from(response.data) }, { quoted: m });
+    } catch (error) {
+        console.error('🍃 Error al generar la imagen:', error);
+        await conn.reply(m.chat, `${msm} No se pudo generar la imagen intenta más tarde.`, m);
+    }
+};
+
+handler.command = ['dalle'];
+handler.help = ['dalle'];
+handler.tags = ['tools'];
+
+export default handler;
